d7edb6dd5ce76303205bf47a6d8b7dde8ebd71ec
Rauhofer <emre.rauhofer@gmail.com>
2026-01-27 14:09:19 +0100

    Added data project for database entries

---
+++ Data/SchichtpilotDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Data;
+
+public class SchichtpilotDbContext : DbContext
+{
+    public SchichtpilotDbContext(DbContextOptions<SchichtpilotDbContext> options) : base(options)
+    {
+        
+    }
+    
+    // Here should be the DBSets
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Add data that should be in the database
+        // Add attributes to model properties like pk, fk, etc..
+        base.OnModelCreating(modelBuilder);
+    }
+}
