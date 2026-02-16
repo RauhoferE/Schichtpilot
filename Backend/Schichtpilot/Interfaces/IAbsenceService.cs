786d53d18e2a7f16418edf2211013d9bd0fb26d7
faribarezai <rezaifariba01@gmail.com>
2026-02-17 00:00:38 +0100

    Added AbsenceService Interface

---
+++ Backend/Schichtpilot/Interfaces/IAbsenceService.cs
@@ -0,0 +1,15 @@
+﻿using Schichtpilot.Models.DTOs;
+
+namespace Schichtpilot.Interfaces;
+
+using Data.Entities;
+
+
+public interface IAbsenceService
+{
+    Task<long> CreateAbsenceRequestAsync(CreateAbsenceDto dto, long userId);
+    Task<List<Absence>> GetMyAbsenceRequestsAsync(long userId);
+    Task DeleteMyAbsenceRequestAsync(long id, long userId);
+    Task<List<Absence>> GetAllAbsenceRequestsAsync(AbsenceFilterDto filter); // Sorting here!
+    Task UpdateStatusAsync(long id, StatusUpdateDto dto);
+}
