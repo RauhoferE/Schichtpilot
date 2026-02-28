b4cfb5e5771e0cb0665160d946eb2d10c9d492e7
faribarezai <rezaifariba01@gmail.com>
2026-02-28 03:26:38 +0100

    updadet to long userid

--- Backend/Schichtpilot/Models/DTOs/AbsenceDto.cs
+++ Backend/Schichtpilot/Models/DTOs/AbsenceDto.cs
@@ -5,13 +5,13 @@ using Schichtpilot.Models.Enums;
 public class AbsenceDto
 {
     public int Id { get; set; }
-    public int UserId { get; set; }
+    public long UserId { get; set; }
     public string UserName { get; set; } = "";
     public DateTime StartDate { get; set; }
     public DateTime EndDate { get; set; }
     public AbsenceTypeEnum AbsenceType { get; set; }
     public string Message { get; set; } = "";
-    public AbsenceStatusEnum Status { get; set; }
+    public AbsenceStatusEnum Status { get; set; } = AbsenceStatusEnum.Pending;
     public DateTime CreatedAt { get; set; }
     public string ManagerMessage { get; set; } = "";
 }
