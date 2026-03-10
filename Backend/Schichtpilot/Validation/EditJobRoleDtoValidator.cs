814422364fad29ccd57e2e7d65af27a36a366e74
Emre Rauhofer <emre.rauhofer@gmail.com>
2026-03-10 11:40:55 +0100

    Added validator for edit job role

---
+++ Backend/Schichtpilot/Validation/EditJobRoleDtoValidator.cs
@@ -0,0 +1,14 @@
+using FluentValidation;
+using Schichtpilot.Models.DTOs;
+
+namespace Schichtpilot.Validation;
+
+public class EditJobRoleDtoValidator : AbstractValidator<EditJobRoleDto>
+{
+    public EditJobRoleDtoValidator()
+    {
+        RuleFor(dto => dto.Name).NotNull().NotEmpty().WithMessage("Name is required");
+        RuleFor(dto => dto.Description).NotNull().NotEmpty().WithMessage("Description is required");
+        RuleFor(dto => dto.Name).MinimumLength(2).MaximumLength(50).WithMessage("Name must be between 2 and 50 characters");
+    }
+}
