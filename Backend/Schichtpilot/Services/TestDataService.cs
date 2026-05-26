5bb22ba12ca2c8c06ffedab76d7f9bba397a3dc2
Rauhofer <emre.rauhofer@gmail.com>
2026-05-26 13:20:18 +0200

    Added create test data service

---
+++ Backend/Schichtpilot/Services/TestDataService.cs
@@ -0,0 +1,120 @@
+using Data;
+using Data.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Schichtpilot.Interfaces;
+using Schichtpilot.Models.DTOs;
+
+namespace Schichtpilot.Services;
+
+//TODO: Remove before prod
+public class TestDataService : ITestDataService
+{
+    public TestDataService(SchichtpilotDbContext dbContext, IUserService userService, IJobRoleService jobRoleService,
+        ICompanyPolicyService companyPolicyService, UserManager<User> userManager)
+    {
+        _dbContext = dbContext ?? throw new ArgumentNullException(nameof(dbContext));
+        _userService = userService ?? throw new ArgumentNullException(nameof(userService));
+        _jobRoleService = jobRoleService ?? throw new ArgumentNullException(nameof(jobRoleService));
+        _companyPolicyService = companyPolicyService ?? throw new ArgumentNullException(nameof(companyPolicyService));
+        _userManager = userManager ?? throw new ArgumentNullException(nameof(userManager));
+    }
+
+    private readonly SchichtpilotDbContext _dbContext;
+    
+    private readonly IUserService _userService;
+    
+    private readonly IJobRoleService _jobRoleService;
+    
+    private readonly ICompanyPolicyService _companyPolicyService;
+    
+    private readonly UserManager<User> _userManager;
+
+    public async Task CreateUsersAsync(int employees, int managers)
+    {
+        for (int i = 0; i < employees; i++)
+        {
+            await this._userService.CreateUserAsync(new UserDto()
+            {
+                AddressDto = new AddressDto()
+                {
+                    City = "Vienna",
+                    PostalCode = 1120,
+                    Street = "Teststraße " + i,
+                },
+                Email = $"user{i}@company.com",
+                FirstName = $"Mia{i}",
+                LastName = $"Test{i}",
+                AssignedJobRoles = new List<JobRoleShortDto>(),
+                Birthdate = DateTime.Now.AddYears(-20),
+            }, "PasswordPassword1!!!");
+        }
+        
+        for (int i = 0; i < managers; i++)
+        {
+            await this._userService.CreateUserAsync(new UserDto()
+            {
+                AddressDto = new AddressDto()
+                {
+                    City = "Vienna",
+                    PostalCode = 1120,
+                    Street = "Teststraße " + i,
+                },
+                Email = $"manager{i}@company.com",
+                FirstName = $"Hannes{i}",
+                LastName = $"Test{i}",
+                AssignedJobRoles = new List<JobRoleShortDto>(),
+                Birthdate = DateTime.Now.AddYears(-20),
+            }, "PasswordPassword1!!!");
+
+            var addedUser = this._dbContext.Users.FirstOrDefault(x => x.Email == $"manager{i}@company.com");
+            if (addedUser != null)
+            {
+                await this._userManager.AddToRoleAsync(addedUser, "ADMIN");
+            }
+            
+        }
+    }
+
+    public async Task CreateRolesAsync()
+    {
+        await this._jobRoleService.CreateJobRoleAsync(new CreateJobRoleDto()
+        {
+            Name = "Waiter",
+            Description = "Waiter",
+            DependentOnJobRoleIds = new List<int>()
+        });
+        
+        await this._jobRoleService.CreateJobRoleAsync(new CreateJobRoleDto()
+        {
+            Name = "Cook",
+            Description = "Cook",
+            DependentOnJobRoleIds = new List<int>()
+        });
+        
+        await this._jobRoleService.CreateJobRoleAsync(new CreateJobRoleDto()
+        {
+            Name = "Cookhelp",
+            Description = "Cookhelp",
+            DependentOnJobRoleIds = new List<int>()
+        });
+        
+        await this._jobRoleService.CreateJobRoleAsync(new CreateJobRoleDto()
+        {
+            Name = "Greeter",
+            Description = "Greeter",
+            DependentOnJobRoleIds = new List<int>()
+        });
+    }
+
+    public async Task CreateWorkPolicyAsync()
+    {
+        await this._companyPolicyService.SetPolicyAsync(new CompanyPolicyDto()
+        {
+            RestPeriodThresholdInMinutes = 30,
+            MinimumRestPeriodInMinutes = 60,
+            MaximumConsecutiveWorkHoursPerDay = 8,
+            MaximumConsecutiveWorkHoursPerWeek = 40
+        });
+    }
+}
